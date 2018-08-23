eaaf8f52d5de3831f5cec20be95c049f9ba43ea9
Gerardo <gherrer1@binghamton.edu>
2018-08-22 22:58:19 -0400

    Deviated a little from Tylers routing structure. Instead of every details route being /details/:city, im gonna do /details/:date

--- app/Components/App.js
+++ app/Components/App.js
@@ -2,11 +2,8 @@ const React = require('react');
 const { BrowserRouter: Router, Route, Switch } = require('react-router-dom');
 const Nav = require('./Nav');
 const Forecast = require('./Forecast');
+const ForecastDetail = require('./ForecastDetail');
 const Home = require('./Home');
-
-function FakeDetail() {
-	return <div>Detail</div>;
-}
 
 function App() {
 	return (
@@ -16,7 +13,7 @@ function App() {
 				<Switch>
 					<Route exact path='/' component={Home} />
 					<Route path='/forecast' component={Forecast} />
-					<Route path='/details' component={FakeDetail} />
+					<Route path='/details/:date' component={ForecastDetail} />
 					<Route component={() => (<div>404</div>)} />
 				</Switch>
 			</div>
---
+++ app/Components/ForecastDetail.js
@@ -0,0 +1,10 @@
+const React = require('react');
+
+function ForecastDetail() {
+	// gets data from props
+	return (
+		<div>Hey</div>
+	);
+}
+
+module.exports = ForecastDetail;
